e1a5113f2fb83fdda6a7718066ed91a4a996c202
Laii Duke <mrlai1011dm@gmail.com>
2022-03-24 15:56:21 +0700

    add lline chart

--- src/main/webapp/app/core/home/home.component.ts
+++ src/main/webapp/app/core/home/home.component.ts
@@ -1,8 +1,13 @@
 import Component from 'vue-class-component';
 import { Inject, Vue } from 'vue-property-decorator';
 import LoginService from '@/account/login.service';
+import LineChart from '@/components/LineChart.vue';
 
-@Component
+@Component({
+  components: {
+    LineChart,
+  },
+})
 export default class Home extends Vue {
   @Inject('loginService')
   private loginService: () => LoginService;
